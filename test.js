ce5c56f0622cd342000844479442a0a9c3b84d53
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-25 01:03:57 -0400

    uncommented test

--- test.js
+++ test.js
@@ -4,16 +4,16 @@ const decode = require('./decode');
 
 
 
-// test('round trip', t => {
-//     const numbers = [];
-//     for (let i = 0; i < 1000; i++) {
-//         numbers.push(Math.round(Math.random() * 100) * 10);
-//     }
-//     console.log("test numbers:", numbers);    
-//     const { char2num, data, nbits, padding } = encode(numbers, { debug: true });
-//     const decoded = decode({ char2num, data, nbits, padding, debug: true });
-//     t.deepEqual(numbers, decoded);
-// });
+test('round trip', t => {
+    const numbers = [];
+    for (let i = 0; i < 1000; i++) {
+        numbers.push(Math.round(Math.random() * 100) * 10);
+    }
+    console.log("test numbers:", numbers);    
+    const { char2num, data, nbits, padding } = encode(numbers, { debug: true });
+    const decoded = decode({ char2num, data, nbits, padding, debug: true });
+    t.deepEqual(numbers, decoded);
+});
 
 test('simple trip', t => {
     const numbers = [920,  550, 340, 690, 550, 340, 840, 700, 550, 210, 540];
